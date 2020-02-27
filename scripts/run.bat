b859a76eaf4849e984121bc9f40bc12c895db994
Andrey Kravchenko <epozine@inbox.ru>
2020-02-27 19:13:02 +0300

    Update maya version

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,4 +2,4 @@ set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set FILE_FILTER=%1
 set TESTS_FILTER="%2"
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir rs2rpr --cmd_variables Tool "C:\Program Files\Autodesk\Maya2018\bin" ResPath "C:\TestResources\RedshiftAssets"
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir rs2rpr --cmd_variables Tool "C:\Program Files\Autodesk\Maya2020\bin" ResPath "C:\TestResources\RedshiftAssets"
